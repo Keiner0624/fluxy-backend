3801f366c897211f53e0da4c3f451466bb566154
chenaoh <cdhenao@gmail.com>
2026-04-19 00:10:36 -0500

    add slug multi-tenant

--- src/main/java/com/fluxyBackend/repository/CompanyRepository.java
+++ src/main/java/com/fluxyBackend/repository/CompanyRepository.java
@@ -3,5 +3,8 @@ package com.fluxyBackend.repository;
 import com.fluxyBackend.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company,Long> {
+    Optional<Company> findBySlug(String slug);
 }
